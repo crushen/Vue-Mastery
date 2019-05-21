b1dd1d390f91749896422e22aed7b5026df352f6
Charlotte <charlotterushen@hotmail.com>
2019-05-21 12:51:00 +0100

    Using v-for to display variantColor

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ const app = new Vue ({
     product: 'Socks',
     image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
     inStock: true,
-    details: ["80% Cotton", "20% Polyester", "Gender-neutral"]
+    details: ["80% Cotton", "20% Polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green'
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue'
+      }
+    ]
   }
 });
