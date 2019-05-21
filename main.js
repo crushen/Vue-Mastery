6e7a34fd9b92d8138c6ef00f6acaafae23bcf5ee
Charlotte <charlotterushen@hotmail.com>
2019-05-21 15:59:22 +0100

    Changing template into a component

--- main.js
+++ main.js
@@ -1,26 +1,54 @@
-const app = new Vue ({
-  el: '#app',
-  data: {
-    brand: 'Vue Mastery',
-    product: 'Socks',
-    selectedVariant: 0,
-    details: ["80% Cotton", "20% Polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: 'green',
-        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-        variantQuantity: 10
-      },
-      {
-        variantId: 2235,
-        variantColor: 'blue',
-        variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
-        variantQuantity: 0
-      }
-    ],
-    cart: 0
-  },
+Vue.component('product', {
+  template: `
+  <div class="product">
+      <div class="product-image">
+        <img v-bind:src="image" alt="">
+      </div>
+      <div class="product-info">
+         <h1>{{ title }}</h1>
+         <p v-if="inStock">In Stock</p>
+         <p v-else>Out of Stock</p>
+         <ul>
+           <li v-for="detail in details">{{ detail }}</li>
+         </ul>
+         <div v-for="(variant, index) in variants" 
+              v-bind:key="variant.variantId"
+              class="color-box"
+              v-bind:style="{backgroundColor: variant.variantColor}"
+              v-on:mouseover="updateProduct(index)">
+         </div>
+         <button v-on:click="addToCart" 
+                 v-bind:disabled="!inStock"
+                 v-bind:class="{disabledButton: !inStock}">Add to cart</button>
+         <div class="cart">
+           <p>Cart: {{ cart }}</p>
+         </div>
+      </div>
+    </div>
+  `,
+  data: function() {
+    return {
+      brand: 'Vue Mastery',
+      product: 'Socks',
+      selectedVariant: 0,
+      details: ["80% Cotton", "20% Polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+          variantQuantity: 10
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+          variantQuantity: 0
+        }
+      ],
+      cart: 0
+    }
+  },   
   methods: {
     addToCart: function() {
       this.cart += 1;
@@ -40,4 +68,10 @@ const app = new Vue ({
       return this.variants[this.selectedVariant].variantQuantity;
     }
   }
+
+});
+
+const app = new Vue ({
+  el: '#app',
+
 });
